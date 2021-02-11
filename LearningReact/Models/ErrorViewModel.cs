25da3fdf27fbdca7b101fbac93a4d0abc35ce4e6
Darin Flippo <darinflippo@gmail.com>
2021-02-10 18:42:28 -0600

    Add project files.

---
+++ LearningReact/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearningReact.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
